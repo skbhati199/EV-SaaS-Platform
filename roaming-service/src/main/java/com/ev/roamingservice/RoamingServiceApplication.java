dbbcb2e85683da42f5a6da7bc3b6e073e7812181
sonu kumar <skbhati199@gmail.com>
2025-04-29 22:43:40 +0530

    🌍 roaming-service: initial commit

---
+++ roaming-service/src/main/java/com/ev/roamingservice/RoamingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ev.roamingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoamingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoamingServiceApplication.class, args);
+    }
+
+}
---
+++ roaming-service/src/test/java/com/ev/roamingservice/RoamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ev.roamingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoamingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
